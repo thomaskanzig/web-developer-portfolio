12e162a92cfb86694b35c5d5c0bd499284ee4174
Thomas Kanzig <thomas.kanzig@gmail.com>
2022-04-16 22:44:14 +0200

    Removed buttons and added icon

--- src/js/components/Hero.tsx
+++ src/js/components/Hero.tsx
@@ -26,20 +26,6 @@ class Hero extends React.Component {
               It is a long established fact that a reader will be distracted by
               the readable content of a page when looking at its layout.
             </p>
-            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg px-4 me-md-2"
-              >
-                Primary
-              </button>
-              <button
-                type="button"
-                className="btn btn-outline-secondary btn-lg px-4"
-              >
-                Default
-              </button>
-            </div>
           </div>
         </div>
       </div>
